7bafe900548c48ca151316c5da008042a2145ab6
alumno <alumno@Ubuntu22EdD.myguest.virtualbox.org>
2024-04-24 19:16:09 +0200

    New Calculadora java class

---
+++ Calculadora1.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.calculadora;
+
+/**
+ *
+ * @author Alumnado
+ */
+public class Calculadora1 {
+
+    private int num1;
+    private int num2;
+    public Calculadora1(int a, int b) {
+    num1 = a;
+    num2 = b;
+    }
+    public int suma() {
+        int result = num1+num2;
+        return result;
+    }
+    public int resta() {
+        int result = num1-num2;
+        return result;
+    }
+    public int multiplica(){
+        int result = num1*num2;
+        return result;
+    }
+    public int divide() {
+        int result = num1/num2;
+        return result;
+    }
+    
+}
